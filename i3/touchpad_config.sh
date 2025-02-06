1aee5720e8fe6543a0ad60f0371a9aa9ecfa7d6b
art2516 <art2516>
2025-02-06 16:54:05 +0100

    Including the initial conf as it is to this day

---
+++ i3/touchpad_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xinput set-prop "ELAN2203:00 04F3:30FA Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "ELAN2203:00 04F3:30FA Touchpad" "libinput Natural Scrolling Enabled" 1
+
---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- init.lua
+
+-- Load core settings
+require("core.colors")
+require("core.options")
+require("core.keymaps")
+
+-- Load plugin management (e.g., packer or another plugin manager)
+require("plugins")
+
+-- Optionally load LSP configurations
+-- require('lsp')
